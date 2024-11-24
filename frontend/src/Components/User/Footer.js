adaf54a3b8ffd3554a2eb27fd392ef3e2f156e1b
prashant-pathak-01 <prashantpathak6395@gmail.com>
2024-11-24 22:47:06 +0530

    Homepage UI Update

--- frontend/src/Components/User/Footer.js
+++ frontend/src/Components/User/Footer.js
@@ -11,19 +11,28 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="bg-opacity-50 backdrop-blur-md bg-gray-900 text-gray-200 py-10 px-4 sm:px-8 md:px-16 lg:px-20">
-      <div className="flex flex-col md:flex-row justify-between items-center md:items-start space-y-10 md:space-y-0">
+      <div className="flex flex-col md:flex-row justify-around items-center">
         {/* Logo and Description */}
         <div className="md:w-1/3 text-center md:text-left">
-        <div className="flex items-center justify-around mb-10">
-          <img src={Logo} alt="Codebusters Logo" className="w-44 mb-4 mx-auto md:mx-0" />
-          <img src={LogoCodechef} alt="Codebusters Logo" className="w-40 mb-4 mx-auto md:mx-0" />
+          <div className="flex items-center justify-around mb-10">
+            <img
+              src={Logo}
+              alt="Codebusters Logo"
+              className="w-44 mb-4 mx-auto md:mx-0 cursor-pointer"
+            />
+            <img
+              src={LogoCodechef}
+              alt="Codebusters Logo"
+              className="w-40 mb-4 mx-auto md:mx-0 cursor-pointer"
+            />
           </div>
-          <p className="text-sm md:text-base px-6 mb-6">
+          <p className="text-sm md:text-base px-6 mb-6 selection:bg-blue-200 selection:text-blue-950">
             Codebusters Club is a community-driven group focused on empowering
             coders with a collaborative environment, exciting challenges, and
-            continuous learning. Join us and be a part of the future of technology.
+            continuous learning. Join us and be a part of the future of
+            technology.
           </p>
-          <p className="text-sm md:text-base px-6">
+          <p className="text-sm md:text-base px-6 selection:bg-blue-200 selection:text-blue-950">
             Be a part of a vibrant community, where collaboration and innovation
             meet to shape the future of technology. Together, we can push the
             boundaries of what’s possible and make a lasting impact on the tech
@@ -32,9 +41,11 @@ function Footer() {
         </div>
 
         {/* Quick Links */}
-        <div className="md:w-1/3 flex flex-col items-center md:items-start ml-52">
-          <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
-          <ul className="space-y-2">
+        <div className="md:w-1/3 w-full flex flex-col items-center justify-center m-10 md:m-0 selection:bg-blue-200 selection:text-blue-950">
+          <h3 className="text-lg font-semibold mb-4 bg-blue-950 pl-6 pr-6 pt-2 pb-2 rounded-lg bg-opacity-30">
+            Quick Links
+          </h3>
+          <ul className="space-y-2 text-slate-300 bg-blue-950 bg-opacity-10 p-4 text-center ml-2 rounded-md">
             <li>
               <Link to="/team-members" className="hover:text-white">
                 Our Team
@@ -64,16 +75,20 @@ function Footer() {
         </div>
 
         {/* Contact Form */}
-        <div className="md:w-1/3 flex flex-col items-center md:items-start">
+        <div className="md:w-1/3 w-full flex flex-col items-center">
           <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
           <form
             action="https://formspree.io/f/myzydpyr"
             method="POST"
             className="bg-slate-700 p-4 rounded-lg shadow-md w-full sm:w-80 md:w-72 lg:w-80"
           >
-            <h2 className="text-xl font-semibold text-white mb-4">Send Us a Message</h2>
+            <h2 className="text-xl font-semibold text-white mb-4">
+              Send Us a Message
+            </h2>
             <label className="block mb-4">
-              <span className="block text-white font-semibold mb-2">Your Email:</span>
+              <span className="block text-white font-semibold mb-2">
+                Your Email:
+              </span>
               <input
                 type="email"
                 name="email"
@@ -83,7 +98,9 @@ function Footer() {
               />
             </label>
             <label className="block mb-4">
-              <span className="block text-white font-semibold mb-2">Your Message:</span>
+              <span className="block text-white font-semibold mb-2">
+                Your Message:
+              </span>
               <textarea
                 name="message"
                 required
@@ -103,12 +120,12 @@ function Footer() {
       </div>
 
       {/* Social Media Links */}
-      <div className="flex justify-center space-x-6 mt-8">
+      <div className="flex flex-row justify-center space-x-6 mt-8 items-center h-10">
         <a
           href="https://www.instagram.com/codebusters_glau/profilecard/?igsh=aWduNzQ0OTAydnpv"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
+          className="text-xl text-gray-400 hover:text-white hover:scale-150 transition-transform"
         >
           <FacebookIcon />
         </a>
@@ -116,7 +133,7 @@ function Footer() {
           href="https://www.instagram.com/codebusters_glau/profilecard/?igsh=aWduNzQ0OTAydnpv"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
+          className="text-xl text-gray-400 hover:text-white hover:scale-150 transition-transform"
         >
           <InstagramIcon />
         </a>
@@ -124,7 +141,7 @@ function Footer() {
           href="https://www.linkedin.com/company/codebusters-glau/"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
+          className="text-xl text-gray-400 hover:text-white hover:scale-150 transition-transform"
         >
           <LinkedInIcon />
         </a>
@@ -132,7 +149,7 @@ function Footer() {
           href="https://twitter.com"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
+          className="text-xl text-gray-400 hover:text-white hover:scale-150 transition-transform"
         >
           <TwitterIcon />
         </a>
@@ -140,15 +157,18 @@ function Footer() {
           href="https://github.com"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
+          className="text-xl text-gray-400 hover:text-white hover:scale-150 transition-transform"
         >
           <GitHubIcon />
         </a>
       </div>
 
       {/* Copyright */}
-      <div className="mt-8 text-center text-gray-500 text-sm">
-        <p>&copy; {new Date().getFullYear()} Codebusters Club. All rights reserved.</p>
+      <div className="mt-8 text-center text-gray-500 text-sm selection:text-blue-100">
+        <p>
+          &copy; {new Date().getFullYear()} Codebusters Club. All rights
+          reserved.
+        </p>
       </div>
     </footer>
   );
--- frontend/src/Components/User/Homepage.js
+++ frontend/src/Components/User/Homepage.js
@@ -14,93 +14,109 @@ function Homepage() {
         {/* Introduction Section */}
         <section className="mt-[7rem] flex flex-col lg:flex-row justify-between items-center p-10 lg:p-20 space-y-10 lg:space-y-0">
           <div className="w-full lg:w-1/2 text-center lg:text-left">
-            <h1 className="text-3xl lg:text-5xl font-extrabold text-gray-300 mb-6 leading-tight font-playfair ">
+            <h1 className="text-3xl lg:text-5xl font-extrabold text-gray-300 mb-6 leading-tight font-playfair bg-opacity-30 bg-black p-2 rounded-lg selection:bg-black selection:text-white">
               Welcome to Codebusters Club!
             </h1>
-            <p className="text-base lg:text-lg text-gray-300 leading-relaxed mx-6 lg:mx-0 mb-6 font-dosis">
+            <p className="text-base lg:text-lg text-gray-300 leading-relaxed mx-6 mb-6 font-dosis bg-black p-2 bg-opacity-20 rounded-lg md:w-2/3 selection:bg-black selection:text-white">
               Codebusters is a community where coders, dreamers, and innovators
               come together to build, learn, and inspire. We believe in the
               power of collaboration and are dedicated to providing a platform
               for everyone—from beginners to pros—to explore their potential.
             </p>
-            <div className="flex gap-10 ">
-            <a
-          href="https://www.instagram.com/codebusters_glau/profilecard/?igsh=aWduNzQ0OTAydnpv"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
-        >
-          <InstagramIcon fontSize="inherit" style={{ fontSize: 40 }} />
-        </a>
-        <a
-          href="https://www.linkedin.com/company/codebusters-glau/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xl text-gray-400 hover:text-white"
-        >
-<LinkedInIcon fontSize="inherit" style={{ fontSize: 40 }} />
-        </a></div>
+            <div className="flex gap-6  md:ml-8 justify-center md:justify-start">
+              <a
+                href="https://www.instagram.com/codebusters_glau/profilecard/?igsh=aWduNzQ0OTAydnpv"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-xl text-gray-400 hover:text-white p-1 bg-black rounded-lg bg-opacity-40"
+              >
+                <InstagramIcon
+                  fontSize="inherit"
+                  style={{
+                    fontSize: 40,
+                  }}
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/company/codebusters-glau/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-xl text-gray-400 hover:text-white p-1 bg-black rounded-lg bg-opacity-40"
+              >
+                <LinkedInIcon fontSize="inherit" style={{ fontSize: 40 }} />
+              </a>
+            </div>
           </div>
           {/* Circular Image */}
-          <div className="w-full lg:w-1/2 flex items-center justify-center">
-          <img
-  src={CB}
-  alt="Codebusters Logo"
-  className="w-96 h-96 lg:w-[400px] lg:h-[400px] rounded-full border-4 border-gray-300 shadow-lg object-cover animate-flip"
-/>
+          <div className="w-full lg:w-1/2 flex items-center justify-center ">
+            <img
+              src={CB}
+              alt="Codebusters Logo"
+              className="md:w-96 md:h-96 lg:w-[400px] lg:h-[400px] rounded-full border-4 border-gray-300 shadow-lg object-cover animate-flip bg-black bg-opacity-50"
+            />
           </div>
         </section>
 
         {/* About Section */}
-        <section className="py-16 px-6 lg:px-20 text-center backdrop-blur-sm bg-opacity-50 bg-blue-200 text-white">
+        <section className="py-16 px-6 lg:px-20 text-center backdrop-blur-lg bg-opacity-50 bg-blue-100 text-white">
           <div className="max-w-7xl mx-auto">
             <h2 className="text-4xl lg:text-4xl font-bold text-blue-950 mb-6">
               About Codebusters Club
             </h2>
-            <p className="text-white text-xl max-w-2xl mx-auto mb-12">
-              Empowering future tech leaders at GLA University through innovation, collaboration, and hands-on coding experiences.
+            <p className="text-blue-950 text-xl max-w-2xl mx-auto mb-12">
+              Empowering future tech leaders at GLA University through
+              innovation, collaboration, and hands-on coding experiences.
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24">
               <div>
-                <h3 className="text-2xl font-semibold text-white mb-4">
+                <h3 className="text-2xl font-bold text-blue-950 mb-4">
                   Our Mission
                 </h3>
-                <p className="text-gray-100 text-lg leading-relaxed">
+                <p className="text-blue-800 text-lg leading-relaxed">
                   At Codebusters Club, we aim to create a nurturing environment
-                  for students passionate about coding and technology. Our mission
-                  is to bridge the gap between theoretical knowledge and practical
-                  skills by providing members with opportunities to participate in
-                  real-world projects, hackathons, and competitions.
+                  for students passionate about coding and technology. Our
+                  mission is to bridge the gap between theoretical knowledge and
+                  practical skills by providing members with opportunities to
+                  participate in real-world projects, hackathons, and
+                  competitions.
                 </p>
               </div>
               <div>
-                <h3 className="text-2xl font-semibold text-white mb-4">
+                <h3 className="text-2xl font-bold text-blue-950 mb-4">
                   Our Core Values
                 </h3>
-                <ul className="text-gray-100 space-y-6">
+                <ul className="text-blue-800 space-y-6">
                   <li className="flex items-center justify-start">
                     <img
-                      src="https://img.icons8.com/ios-filled/50/4e92f9/innovation.png"
+                      src="https://img.icons8.com/?size=100&id=aMa5RfFHeMJg&format=png&color=000000"
                       alt="Innovation Icon"
                       className="h-8 mr-4"
                     />
-                    <span className="text-lg">Innovation - Encouraging creative problem-solving and fresh ideas.</span>
+                    <span className="text-lg">
+                      Innovation - Encouraging creative problem-solving and
+                      fresh ideas.
+                    </span>
                   </li>
                   <li className="flex items-center justify-start">
                     <img
-                      src="https://img.icons8.com/ios-filled/50/4e92f9/collaboration.png"
+                      src="https://img.icons8.com/?size=100&id=4l0HjwFFbVRJ&format=png&color=000000"
                       alt="Collaboration Icon"
                       className="h-8 mr-4"
                     />
-                    <span className="text-lg">Collaboration - Building a strong community through teamwork.</span>
+                    <span className="text-lg">
+                      Collaboration - Building a strong community through
+                      teamwork.
+                    </span>
                   </li>
                   <li className="flex items-center justify-start">
                     <img
-                      src="https://img.icons8.com/ios-filled/50/4e92f9/growth.png"
+                      src="https://img.icons8.com/?size=100&id=16Ne6ZK7nvPI&format=png&color=000000"
                       alt="Growth Icon"
                       className="h-8 mr-4"
                     />
-                    <span className="text-lg">Growth - Supporting personal and professional development.</span>
+                    <span className="text-lg">
+                      Growth - Supporting personal and professional development.
+                    </span>
                   </li>
                 </ul>
               </div>
